c4f5a8782d5d42b2ce38505528ef0ef2e188418e
kyrozepto <108352285+kyrozepto@users.noreply.github.com>
2023-12-07 09:28:49 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-	$host = 'localhost';
-	$user = 'root';
-	$pass ='';
+	$host = getenv('DATABASE_HOST');
+	$user = getenv('DATABASE_USER');
+	$pass = getenv('DATABASE_PASSWORD');
 	$db = 'db_ilmu';
 	$conn = mysqLi_connect($host,$user, $pass, $db);
 	if($conn){
